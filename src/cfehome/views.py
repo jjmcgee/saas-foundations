20610f62873c8cf03b9a0017a35e469d04e3e4be
John McGee <jjmcgee@me.com>
2024-06-28 15:14:00 +0100

    fixed views

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -15,7 +15,7 @@ def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     try:
-        percent =  (page_qs.count() * 100.0) / qs.count()
+        percent = (page_qs.count() * 100.0) / qs.count()
     except:
         percent = 0
     my_title = "My Page"
@@ -24,7 +24,7 @@ def about_view(request, *args, **kwargs):
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "percent": percent,
-        "total_visit_count": qs.count()
+        "total_visit_count": qs.count(),
     }
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
