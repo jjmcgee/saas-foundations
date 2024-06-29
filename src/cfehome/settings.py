e3a482ad924c5c2212962cab52a2df41addcddee
John McGee <jjmcgee@me.com>
2024-06-29 09:53:20 +0100

    added whitenoise

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -152,6 +153,13 @@ STATICFILES_DIRS = [
 # output for python manage.py collectstaticfiles
 # local cdn
 STATIC_ROOT = BASE_DIR / "local-cdn"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
