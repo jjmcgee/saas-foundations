ed222e2ff190696d53596ff099f8264a43fea924
John McGee <jjmcgee@me.com>
2024-11-08 15:04:06 +0000

    updating repo to continue

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -101,7 +101,7 @@ if DATABASE_URL is not None:
     DATABASES = {
         "default": dj_database_url.config(
             default=DATABASE_URL, 
-            conn_max_age=CONN_MAX_AGE, 
+            conn_max_age=CONN_MAX_AGE,
             conn_health_checks=True,
         )
     }
